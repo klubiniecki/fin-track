352753498a9fedf280116bc7fd70edab3099b74c
klubiniecki <klubiniecki@gmail.com>
2020-05-01 08:39:58 +1000

    update icons

--- src/components/Transaction/TransactionCategories.tsx
+++ src/components/Transaction/TransactionCategories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "@material-ui/icons/HomeTwoTone";
+import HouseTwoToneIcon from "@material-ui/icons/HouseTwoTone";
 import ShoppingBasket from "@material-ui/icons/ShoppingBasketTwoTone";
 import InsertEmoticon from "@material-ui/icons/InsertEmoticonOutlined";
 import FavoriteBorder from "@material-ui/icons/FavoriteTwoTone";
@@ -12,6 +12,11 @@ import OtherIncome from "@material-ui/icons/RedeemOutlined";
 import Salary from "@material-ui/icons/CreditCardOutlined";
 import Sales from "@material-ui/icons/MonetizationOnTwoTone";
 import Saving from "@material-ui/icons/SaveTwoTone";
+import DomainDisabledTwoToneIcon from "@material-ui/icons/DomainDisabledTwoTone";
+import EmojiFoodBeverageTwoToneIcon from "@material-ui/icons/EmojiFoodBeverageTwoTone";
+import LocalLaundryServiceTwoToneIcon from "@material-ui/icons/LocalLaundryServiceTwoTone";
+import LocalSeeTwoToneIcon from "@material-ui/icons/LocalSeeTwoTone";
+import SubscriptionsTwoToneIcon from "@material-ui/icons/SubscriptionsTwoTone";
 import { TransactionCategory, TransactionType } from "../../types/types";
 
 export interface TransactionCategoryAndIcon {
@@ -22,18 +27,30 @@ export interface TransactionCategoryAndIcon {
 
 const TransactionCategories: TransactionCategoryAndIcon[] = [
   { name: "Groceries", icon: <ShoppingBasket />, type: "expense" },
-  { name: "Rent", icon: <Home />, type: "expense" },
+  { name: "Rent", icon: <HouseTwoToneIcon />, type: "expense" },
   { name: "Fun", icon: <InsertEmoticon />, type: "expense" },
   { name: "Gaia", icon: <FavoriteBorder />, type: "expense" },
   { name: "Car", icon: <DirectionsCar />, type: "expense" },
   { name: "Medical", icon: <LocalHospital />, type: "expense" },
   { name: "Wellness", icon: <ColorLens />, type: "expense" },
   { name: "Travel", icon: <FlightTakeoff />, type: "expense" },
-  { name: "Utilities", icon: <OtherExpense />, type: "expense" },
-  { name: "Restaurant", icon: <OtherExpense />, type: "expense" },
-  { name: "Household", icon: <OtherExpense />, type: "expense" },
-  { name: "Clothes", icon: <OtherExpense />, type: "expense" },
-  { name: "Subscriptions", icon: <OtherExpense />, type: "expense" },
+  { name: "Utilities", icon: <DomainDisabledTwoToneIcon />, type: "expense" },
+  {
+    name: "Restaurant",
+    icon: <EmojiFoodBeverageTwoToneIcon />,
+    type: "expense",
+  },
+  {
+    name: "Household",
+    icon: <LocalLaundryServiceTwoToneIcon />,
+    type: "expense",
+  },
+  { name: "Clothes", icon: <LocalSeeTwoToneIcon />, type: "expense" },
+  {
+    name: "Subscriptions",
+    icon: <SubscriptionsTwoToneIcon />,
+    type: "expense",
+  },
   { name: "Other", icon: <OtherExpense />, type: "expense" },
   { name: "Salary", icon: <Salary />, type: "income" },
   { name: "Sales", icon: <Sales />, type: "income" },
